9a617a95b4ce7d204b2eac3ab4f5b0ecb2ad0008
Tie Ying Kathleen <tieyingkathleen@gmail.com>
2019-08-19 12:31:06 +0800

    add Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        String[] list = new String[100];
 
         /**String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -15,11 +16,31 @@ public class Duke {
         System.out.println("Hello! I'm Duke\n");
         System.out.println("What can I do for you?");
 
-        String command = input.next();
+        String command = input.nextLine();
+        // n is the no. of items in the list
+        int n = 0;
         while (!(command.equals("bye"))) {
-            System.out.println(command);
-            command = input.next();
+            if (command.equals("list")) {
+                printList(list,n);
+            }
+            // commands that are not "bye" or "list" are added into list
+            else {
+                String newItem = command;
+                list[n] = newItem;
+                System.out.println("added: "+newItem);
+                n++;
+            }
+            command = input.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
+
+    //Prints all list items in the string array
+    private static void printList(String[] list, int n) {
+        for (int i=0; i<n; i++) {
+            int listNum = i+1;
+            System.out.println(listNum+". "+list[i]);
+        }
+    }
+
 }
