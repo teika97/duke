c72d0f6d65ce1c5de9812eae433eec46b3dfbe55
Tie Ying Kathleen <tieyingkathleen@gmail.com>
2019-08-19 14:45:19 +0800

    add Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String[] list = new String[100];
+        Task[] list = new Task[100];
 
         /**String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -20,14 +20,23 @@ public class Duke {
         // n is the no. of items in the list
         int n = 0;
         while (!(command.equals("bye"))) {
+            // calls method to print all tasks in list
             if (command.equals("list")) {
                 printList(list,n);
             }
-            // commands that are not "bye" or "list" are added into list
+            // marks tasks as done for the done command
+            else if (command.contains("done")) {
+                String [] cmdBreakDown = command.split(" ");
+                int itemNo = Integer.parseInt(cmdBreakDown[1]) - 1;
+                list[itemNo].isDone = true;
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("["+list[itemNo].getStatusIcon()+"] "+list[itemNo].description);
+            }
+            // all other commands will be added into the list as a new task
             else {
-                String newItem = command;
+                Task newItem = new Task(command);
                 list[n] = newItem;
-                System.out.println("added: "+newItem);
+                System.out.println("added: "+newItem.description);
                 n++;
             }
             command = input.nextLine();
@@ -36,11 +45,13 @@ public class Duke {
     }
 
     //Prints all list items in the string array
-    private static void printList(String[] list, int n) {
+    private static void printList(Task[] list, int n) {
+        System.out.println("Here are the tasks in your list:");
         for (int i=0; i<n; i++) {
             int listNum = i+1;
-            System.out.println(listNum+". "+list[i]);
+            System.out.println(listNum+". "+"["+list[i].getStatusIcon()+"] "+list[i].description);
         }
     }
 
 }
+
